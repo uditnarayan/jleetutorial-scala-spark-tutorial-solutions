b8e2988c87fde752615b528550286fe43ca455bf
Udit Narayan <info.udit90@gmail.com>
2018-07-30 00:36:20 +0530

    Reduce, sum of numbers solved

--- src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
@@ -1,5 +1,8 @@
 package com.sparkTutorial.rdd.sumOfNumbers
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SumOfNumbersProblem {
 
   def main(args: Array[String]) {
@@ -9,5 +12,21 @@ object SumOfNumbersProblem {
 
        Each row of the input file contains 10 prime numbers separated by spaces.
      */
+    Logger.getLogger("org").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("SumOfNumbers").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile("in/prime_nums.text")
+    val numbers = data.flatMap(line => line.split("\\s+"))
+
+    // I missed this in my initial solution and it causes
+    // issues as there are empty string in the RDD.
+    val filtered = numbers.filter(num => !num.isEmpty)
+
+    val integers = filtered.map(num => num.toInt)
+    val sum = integers.reduce((a,b) => a+b)
+
+    println("Sum: " + sum)
   }
 }
