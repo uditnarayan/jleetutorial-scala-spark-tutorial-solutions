bf8e79bcb713188b8a9731a04ddef6cb0e9d8e3d
Udit Narayan <info.udit90@gmail.com>
2018-07-29 13:14:02 +0530

    Airports by latitude solution.

--- src/main/scala/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.scala
@@ -1,5 +1,8 @@
 package com.sparkTutorial.rdd.airports
 
+import com.sparkTutorial.commons.Utils
+import org.apache.spark.{SparkConf, SparkContext}
+
 object AirportsByLatitudeProblem {
 
   def main(args: Array[String]) {
@@ -16,5 +19,17 @@ object AirportsByLatitudeProblem {
        "Tofino", 49.082222
        ...
      */
+    val conf = new SparkConf().setAppName("airports").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    val airports = sc.textFile("in/airports.text")
+    val airportsInUSA = airports.filter(line => line.split(Utils.COMMA_DELIMITER)(6).toFloat > 40)
+
+    val airportsNameAndCityNames = airportsInUSA.map(line => {
+      val splits = line.split(Utils.COMMA_DELIMITER)
+      splits(1) + ", " + splits(6)
+    })
+    airportsNameAndCityNames.saveAsTextFile("out/airports_by_latitude_soln.text")
+
   }
 }
